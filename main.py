07f5572a113dadbbde03aaff3d73ec428006651c
CarlosBarbozaE <31021923+CarlosBarbozaE@users.noreply.github.com>
2021-03-13 13:36:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ sns.set()
 TEXTO = input("Introduce el ticker del activo que quieras visualizar. ")
 activo = TEXTO
 grafica = ft.calculate_moving_average_crossovers(activo)
+estrategiasmi = ft.smivisual(activo)
+estrategiasmi2 = ft.smiestrategia(activo)
+estrategiamacd = ft.macdestrategia(activo)
+estrategiarsi = ft.estrategia_rsi(activo)
+resistencias = ft.soporteresistencia(activo)
